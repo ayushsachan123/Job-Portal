c80e82ee7f76640de29654fcc6675b9e82139d52
ayushsachan123 <ayushsachan.in@gmail.com>
2024-03-11 03:13:40 +0530

    edit swagger

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ const options = {
     },
     servers: [
       {
-//         url: "http://localhost:8080",
-            url: "https://nodejs-job-portal-app.onrender.com"
+        url: "http://localhost:8080",
+            // url: "https://nodejs-job-portal-app.onrender.com"
       },
     ],
   },
